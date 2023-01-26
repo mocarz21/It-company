0ce3795f908e02779037d3119a0dac997794db19
Adam Mocarski <adammocarski22@gmail.com>
2023-01-26 11:49:29 +0100

    bug with render

--- src/components/features/Poziom.js
+++ src/components/features/Poziom.js
@@ -1,38 +1,46 @@
 
 
-    const Poziom =({id, topic})=>{
+const   Poziom =({id, topic})=>{
 
-        let lvls= []
-        let program = ''
-        
+    let lvls= [];
+    
         for(let key in topic.lvl){
-            lvls.push(key)
-           let program = topic.lvl[key]
-        
-        }
+        lvls.push(key)
 
-        const show =()=>{
+    }
+    const cbd= document.querySelector(".box-lvlProgram")
+    console.log(cbd)
+    const  show =(e)=>{ 
+        e.preventDefault()
+        const abc= document.querySelector(".box-lvlProgram")
+        console.log(abc)
 
-           // const programLocalization = document.querySelector('.'+ program);
-           // programLocalization.classList.toggle('active')
-          //  console.log(programLocalization)
-        }
+        // if(document.querySelector('.'+ lv) ==='.'){
+        //     console.log('niema')
+        // }else{
+        //      const programLocalization = document.querySelector('.'+ lv);
+        //      programLocalization.classList.toggle('active')
+        //      console.log(programLocalization)
+        // }
 
-
-        return(
-            lvls.map(lv=>
-                <div key={id+lv} classname='box-lvlProgram'>
-                    <button onClick={show}>
-                        <div  onClick={show('program'+ lv)} className = {'lvl-'+ id}>
-                            <h2>{lv}</h2>
-                        </div>
-                    </button>
-                    <div  className={program}>
-                        <h3>{program}</h3>
-                    </div>
-                </div>
-            )
-        )
     }
 
-    export default Poziom;
+    return(
+        <>
+            {lvls.map(lv=>
+            <div key={id+lv} classname='box-lvlProgram'>
+                <button onClick={show}>
+                    <div className = {'lvl-'+ id}>
+                        <h2>{lv}</h2>
+                    </div>
+                </button>
+                <div  className={lv}>
+                    <h3>{topic.lvl[lv]}</h3>
+                </div>
+            </div>
+        )}
+        </> 
+    )
+}
+
+export default Poziom;
